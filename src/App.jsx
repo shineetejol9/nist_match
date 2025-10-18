ea959c8956dec91a4db3bc65dc28aa8baae41446
shineetejol9 <shineetejolpatro@gmail.com>
2025-10-18 20:54:28 +0530

    fix

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { useEffect } from "react";
+import { gsap } from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Header from "./components/Header.jsx";
+import HeroSection from "./components/HeroSection.jsx";
+import CustomCursor from "./components/CustomCursor.jsx";
+import Aboutsection from "./components/Aboutsection.jsx";
+import ProjectsSection from "./components/ProjectsSection.jsx";
+import ContactSection from "./components/ContactSection.jsx";
+
+
+export default function App() {
+
+  useEffect(() => {
+    // register scrollTrigger plugin
+    gsap.registerPlugin(ScrollTrigger)
+
+    // Refresh scrollTrigger when the page is fully loaded
+    ScrollTrigger.refresh()
+
+    // clean up ScrollTrigger on component unmount
+    return() => {
+      ScrollTrigger.getAll().forEach((trigger) => trigger.kill ())
+    }
+    
+  },[])
+
+  return (
+    <>
+      <CustomCursor />
+      <Header />
+      <HeroSection />
+      <Aboutsection />
+      <ProjectsSection />
+      <ContactSection />
+      
+
+    </>
+  );
+}
