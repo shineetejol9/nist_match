bef23f3f67faef51b51f18c210c0d2e90b0c54a2
shineetejol9 <shineetejolpatro@gmail.com>
2025-10-19 12:06:14 +0530

    Fix merge conflicts and update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import { useEffect } from "react";
-import { gsap } from "gsap"
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import Header from "./components/Header.jsx";
-import HeroSection from "./components/HeroSection.jsx";
-import CustomCursor from "./components/CustomCursor.jsx";
-import AboutSection from "./components/AboutSection.jsx";
-import ProjectsSection from "./components/ProjectsSection.jsx";
-import ContactSection from "./components/ContactSection.jsx";
-
-
-export default function App() {
-
-  useEffect(() => {
-    // register scrollTrigger plugin
-    gsap.registerPlugin(ScrollTrigger)
-
-    // Refresh scrollTrigger when the page is fully loaded
-    ScrollTrigger.refresh()
-
-    // clean up ScrollTrigger on component unmount
-    return() => {
-      ScrollTrigger.getAll().forEach((trigger) => trigger.kill ())
-    }
-    
-  },[])
-
-  return (
-    <>
-      <CustomCursor />
-      <Header />
-      <HeroSection />
-      <AboutSection />
-      <ProjectsSection />
-      <ContactSection />
-      
-
-    </>
-=======
 import React, { useEffect } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -46,7 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header.jsx";
 import HeroSection from "./components/HeroSection.jsx";
-import Aboutsection from "./components/Aboutsection.jsx";
+import AboutSection from "./components/AboutSection.jsx"; // fixed naming
 import ProjectsSection from "./components/ProjectsSection.jsx";
 import ContactSection from "./components/ContactSection.jsx";
 import BlankPage from "./components/BlankPage.jsx";
@@ -56,6 +15,7 @@ export default function App() {
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
     ScrollTrigger.refresh();
+
     return () => {
       ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
     };
@@ -72,7 +32,7 @@ export default function App() {
             <>
               <Header />
               <HeroSection />
-              <Aboutsection />
+              <AboutSection />
               <ProjectsSection />
               <ContactSection />
             </>
@@ -83,6 +43,5 @@ export default function App() {
         <Route path="/blank" element={<BlankPage />} />
       </Routes>
     </Router>
->>>>>>> temp-backup
   );
 }
